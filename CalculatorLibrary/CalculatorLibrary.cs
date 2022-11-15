21d8107bc70814260170b03625e288e97aa95824
alexandra_rynkevich <alexandrar@playtika.com>
2022-11-15 16:14:26 +0100

    added new Calculator Library

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
+
+namespace Cat
+{
+    public class Jocker
+    {
+        public static string MyCat(string name)
+        {
+            return "Hello World";
+        }
+    }
+}
